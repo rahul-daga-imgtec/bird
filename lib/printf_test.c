1c7f4208eb4770d17d585d34f45adf9371164bc1
Pavel Tvrdik <pawel.tvrdik@gmail.com>
2016-08-29 13:33:56 +0200

    printf_test: post-merge fix
    The variable bt_test_suite_success is managed by unit test framework
    using bt_assert* functions.
    
    bt_assert_msg() functions show a message each time while bad result and
    even while good result too.

--- lib/printf_test.c
+++ lib/printf_test.c
@@ -1,5 +1,5 @@
 /*
- *	BIRD Library -- Generic Bit Operations Tests
+ *	BIRD Library -- String Functions Tests
  *
  *	(c) 2015 CZ.NIC z.s.p.o.
  *
@@ -7,7 +7,6 @@
  */
 
 #include "test/birdtest.h"
-#include "test/bt-utils.h"
 
 #include "lib/string.h"
 
@@ -15,15 +14,13 @@
   do { \
     int n = bsprintf(buf, fmt, ##__VA_ARGS__); \
     bt_assert_msg(n == nw, "fmt=\"%s\" returns n=%d, want %d", fmt, n, nw); \
-    bt_assert_msg(buf[n] == 0, "fmt=\"%s\" doesn't make buf[%d]=\'\\0\', found 0x%02x instead", fmt, n, buf[n]); \
+    bt_assert_msg(buf[n] == 0, "fmt=\"%s\" buf[%d] should be \'\\0\', found 0x%02x", fmt, n, buf[n]); \
     bt_assert_msg(memcmp(buf, res, nw) == 0, "fmt=\"%s\" writes \"%*s\", want \"%*s\"", fmt, (n < nw ? n : nw), buf, nw, res); \
   } while (0)
 
 static int
 t_simple(void)
 {
-  bt_test_suite_success = BT_SUCCESS;
-
   char buf[256];
   memset(buf, 0xa5, 256);
 
@@ -57,7 +54,7 @@ t_simple(void)
   BSPRINTF(2, "-1", buf, "%d", -1);
   BSPRINTF(11, "-2147483648", buf, "%d", -2147483648);
 
-  return bt_test_suite_success;
+  return BT_SUCCESS;
 }
 
 int
@@ -67,5 +64,5 @@ main(int argc, char *argv[])
 
   bt_test_suite(t_simple, "printf without varargs");
 
-  return bt_end();
+  return bt_exit_value();
 }
